a9182a922d6368f54d23da622d937c4f132175e5
a sullon m <asullom@gmail.com>
2020-10-30 08:23:40 -0500

    eventos crud

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="appx">
+    <p> HI </p>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="hOLA "  msg2="hOLA2 " message="xxxx "/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+.appx {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p> 
+      {{ msg2 }}
+      {{ message }}
+  <input v-model="msg">
+
+
+    </p>
+
+    <button
+                          type="button"
+                          class="btn btn-warning btn-sm"
+                         
+                          @click="editBook()">
+                      Update
+                  </button>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: function() {
+    return {
+      message: "gi", num2: ""
+    }
+  },   
+  props: {
+    msg: String,
+    msg2: String,
+    
+  },
+  methods: {
+    getBooks() {
+      const path = 'Hola has create';
+      console.log(path);
+        
+    },
+    addBook(payload) {
+      const path = 'http://localhost:5001/books';
+      console.log(path+payload);
+    },
+    editBook() {
+      console.log("edit ");
+    },
+
+  },
+  created() {
+    this.getBooks();
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.hello {
+color: #42b983;
+}
+
+</style>
