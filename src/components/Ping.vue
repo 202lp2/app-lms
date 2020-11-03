13bcecc6f90f72961cf4829da9994753e56182a8
a sullon m <asullom@gmail.com>
2020-11-03 08:40:37 -0500

    router is integrated

---
+++ src/components/Ping.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button type="button" class="btn btn-primary">{{ msg }}</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Ping',
+  data: function() {
+    return {
+      msg: "ping"
+    }
+  },   
+  methods: {
+    getMessage: function() {
+      const path = 'Pong';
+      console.log(path);
+    },
+  },
+  created: function() {
+    this.getMessage();
+  }, 
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 //import Books from './components/Books.vue';
-//import Ping from './components/Ping.vue';
+import Ping from './components/Ping.vue';
 import HelloWorld from './components/HelloWorld.vue'
+
+
 Vue.use(Router);
 
 export default new Router({
@@ -14,6 +16,11 @@ export default new Router({
       name: 'HelloWorld',
       component: HelloWorld,
     },
-    
+    {
+      path: '/ping',
+      name: 'Ping',
+      component: Ping,
+    },
+
   ],
 });
